3474ba053a6e3f82496bd08d767089b115bed24a
EE18B137 <57704385+Techenthusiast2001@users.noreply.github.com>
2022-11-15 21:42:04 +0530

    Update bbox_ref_model.py

--- bbox_verif/bbox_ref_model.py
+++ bbox_verif/bbox_ref_model.py
@@ -96,6 +96,158 @@ def bbox_rm(instr, rs1, rs2, XLEN):
             i += 1
        res = (res // 2**(XLEN-1)) % 2**XLEN
        valid = '1'
+    elif instr == 0b01100000000000000001000000010011:
+       res = 0
+       for k in range(XLEN-1, -1, -1):
+           if(rs1 >= 2**k):
+                res = XLEN-k-1 
+                break
+       valid = '1'
+    elif instr == 0b01100000000000000001000000011011:
+        res = 0
+        for k in range(31, -1, -1):
+          if(rs1 % 2**32 >= 2**k):
+             res = 31-k
+             break
+        valid = '1'
+    elif instr == 0b01100000001000000001000000010011:
+      rs1 = str(bin(rs1))
+      res = 0
+      for i in rs1:
+         if i == "1":
+            res+=1
+      valid = '1'
+    elif instr == 0b01100000001000000001000000011011:
+      rs1 = str(bin(rs1 % 2**32))
+      res = 0
+      for i in rs1:
+         if i == "1":
+            res+=1
+      valid = '1'
+    elif instr == 0b01100000000100000001000000010011:
+        res = 0
+        for k in range(XLEN-1, -1, -1):
+          if(rs1 % 2**k == 0):
+             res = k 
+             break
+        valid = '1'
+    elif instr == 0b01100000000100000001000000011011:
+        res = 0
+        for k in range(31, -1, -1):
+          if(rs1 % 2**k == 0):
+             res = k 
+             break
+        valid = '1'
+    elif instr == 0b00001010000000000110000000110011:#MAX
+        sign1 = 0; sign2 = 0; res = rs1
+        if(rs1 > 2**(XLEN-1)):
+           sign1 = 1
+        if(rs2 > 2**(XLEN-1)):
+           sign2 = 1    
+        if(sign1==sign2):
+            res = max(rs1, rs2)
+        else:
+            res = min(rs1, rs2)
+        valid = '1'
+    elif instr == 0b00001010000000000111000000110011:
+        res = max(rs1, rs2);
+        valid = '1'
+    elif instr == 0b00001010000000000100000000110011:
+        sign1 = 0; sign2 = 0; res = 0
+        if(rs1 > 2** (XLEN-1)):
+           sign1 = 1
+        if(rs2 > 2** (XLEN-1)):
+           sign2 = 1
+        if(sign1==sign2):
+            res = min(rs1, rs2)
+        else:
+            res = max(rs1, rs2)
+        valid = '1'
+    elif instr == 0b00001010000000000101000000110011:#MINU
+        res = min(rs1, rs2);
+        valid = '1'     
+    elif instr == 0b00101000011100000101000000010011:#ORC_B
+        res = 0
+        for i in range(0, XLEN, 8):
+          if (rs1 // 2**i) % 2**8 > 0:
+              res = res + 2**i *255
+        valid = '1'
+    elif instr == 0b01000000000000000110000000110011:
+        res = (rs1 | ~rs2) % 2**XLEN
+        valid = '1'
+    elif(((instr == 0b01101001100000000101000000010011) and XLEN ==32) or ((instr == 0b01101011100000000101000000010011) and XLEN==64)):#REV8
+        res = 0
+        if(XLEN==64):
+        	rint = str('{:064b}'.format(rs1))
+        else:
+        	rint = str('{:032b}'.format(rs1)) 
+        for i in range(0, XLEN, 8):
+              res += int('0b' + (rint[i:i+8][::-1]), 2) *2**(XLEN-8-i)
+        valid = '1'
+    elif instr == 0b01100000000000000001000000110011:#ROL
+        shamt = rs2 % 2**XLEN_log
+        res1 = (rs1 // 2**(XLEN-shamt))
+       # res1 = 2**shamt - 1 - (rs1 // 2**(XLEN-shamt))   #highest shamt bits needed in reverse order
+        res2 = (rs1 % 2**(XLEN-shamt)) * 2**shamt
+        res = res1 + res2
+        valid = '1'
+    elif (instr == 0b01100000000000000001000000111011 and XLEN==64):#ROLW
+        rs1 = rs1 % 2**32
+        shamt = rs2 % 2**5
+        res1 = (rs1 // 2**(32-shamt)) 
+        res2 = (rs1 % 2**(32-shamt)) * 2**shamt
+        res = res1 + res2
+        if(res >= 2**31):
+            res += (2**XLEN-2**32)
+        valid = '1'
+    elif instr == 0b01100000000000000101000000110011: #ROR
+        shamt = rs2 % 2**XLEN_log
+        #res1 = (2**shamt - 1- (rs1 % 2**(shamt))) * 2**(XLEN-shamt)   #smallest shamt bits reversed and shifted up
+        res1 = (rs1 % 2**(shamt))* 2**(XLEN-shamt)
+        res2 = rs1 // 2**shamt
+        res = res1 + res2
+        valid = '1'
+    elif ((bin(instr)[-31:-26] == '11000') and (bin(instr)[-15:-12] == '101') and (bin(instr)[-7:] == '0010011') and XLEN==64):#RORI-64
+        shamt = int(bin(instr)[-26:-20], 2)
+        res1 = (rs1 % 2**(shamt))* 2**(XLEN-shamt)
+        res2 = rs1 // 2**shamt
+        res = res1 + res2
+        valid = '1'
+    elif ((bin(instr)[-31:-25] == '110000') and (bin(instr)[-15:-12] == '101') and (bin(instr)[-7:] == '0010011') and XLEN==32):#RORI-32
+        shamt = int(bin(instr)[-25:-20], 2)
+        res1 = (rs1 % 2**(shamt))* 2**(XLEN-shamt)
+        res2 = rs1 // 2**shamt
+        res = res1 + res2
+        valid = '1'
+    elif ((bin(instr)[-31:-25] == '110000') and (bin(instr)[-15:-12] == '101') and (bin(instr)[-7:] == '0011011') and XLEN==64):#RORIW 
+        shamt = int(bin(instr)[-25:-20], 2)
+        rs1 = rs1 % 2**32
+        res1 = (rs1 % 2**(shamt))* 2**(32-shamt)
+        res2 = rs1 // 2**shamt
+        res = res1 + res2
+        if(res >= 2**31):
+          res += (2**XLEN - 2**32)
+        valid = '1'
+    elif (instr == 0b01100000000000000101000000111011 and XLEN==64): #RORW
+        rs1 = rs1 % 2**32
+        shamt = rs2 % 2**5
+        #res1 = (2**shamt - 1- (rs1 % 2**(shamt))) * 2**(XLEN-shamt)   #smallest shamt bits reversed and shifted up
+        res1 = (rs1 % 2**(shamt))* 2**(32-shamt)
+        res2 = rs1 // 2**shamt
+        res = (res1 + res2)
+        if(res >= 2**31):
+           res+= (2**XLEN - 2**32)
+        valid = '1'
+    elif instr == 0b01100000010000000001000000010011:#SEXT_B
+       res = rs1 % 2**8
+       if (res >= 2**7):
+          res+=(2**XLEN -1 - 255)
+       valid = '1'
+    elif instr == 0b01100000010100000001000000010011:#SEXT_H
+       res = rs1 % 2**16
+       if(res >= 2**15):
+          res+=(2**XLEN - 2**16)
+       valid = '1'
     elif instr == 0b00100000000000000010000000110011:#SH1ADD
        res = (rs2 + (rs1 << 1)) % 2**XLEN#rs1 is shifted left by 1 position and then added with rs2
        valid = '1'
